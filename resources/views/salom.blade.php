203b56dc15ff7670bd7d02b9cb4fe9193f210c4b
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2020-09-17 12:59:29 -0400

    My first push of the project

---
+++ resources/views/salom.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Laravel Dunyosidan sizga Salom!</h1>
+<hr>
+<img class="myimg" src="https://cdn2.wanderlust.co.uk/media/1014/skyline-2.jpg?anchor=center&mode=crop&width=600&height=225&format=auto&quality=30&rnd=132155311170000000">
+<style>
+    .myimg{
+        width: 70%;
+    }
+</style>
+</body>
+</html>
